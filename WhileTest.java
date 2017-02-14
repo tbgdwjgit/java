9beff013c9cf8f61b1546e42bcdc86376e771b72
tbgdwj <17049185@qq.com>
2017-02-14 17:20:36 +0800

    add WhileTest.java

---
+++ WhileTest.java
@@ -0,0 +1,9 @@
+public class WhileTest{
+	public static void main(String args[]){
+        int a=100;      
+        while(a%2==0){        
+            System.out.print(a);        
+            a=a/4;     
+        }	
+    }
+}//100
