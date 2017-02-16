5f3ccf161890cbd546f6414b12e7e98ac23e7c3e
tbgdwj <17049185@qq.com>
2017-02-16 10:53:13 +0800

    add ArrayTest.java

---
+++ ArrayTest.java
@@ -0,0 +1,16 @@
+public class ArrayTest{
+	public static void main(String args[]){
+        int []a[]  = new int[10][10];
+        int a1[][] = new int[10][10];     
+        int [][]a2 = new int[10][10];
+        int[][] a3 = new int[10][10]; 
+        int a4[] = new int[10];
+
+        for(int i=0 ;i < a4.length ; i++){
+        	System.out.println(a4[i]);
+        }
+
+	}
+
+
+}
