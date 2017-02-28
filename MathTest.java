60fa4033616a9f1cc5e7d1d856bf7ca23e54115e
tbgdwj <17049185@qq.com>
2017-02-28 17:32:35 +0800

    modify

--- MathTest.java
+++ MathTest.java
@@ -1,6 +1,35 @@
+//import java.lang.Math;
+import java.util.Random;
+
 public class MathTest{
 	public static void main(String args[]){
-		//
+
+		//Math
+        System.out.println(Math.E);
+        System.out.println(Math.PI);
+        System.out.println(Math.max(53,34));
+        System.out.println(Math.min(53,34));
+        System.out.println(Math.round(53.4499999));
+        System.out.println("                    ");
+
+        //java.util.Random
+        Random rand = new Random(20);
+        System.out.println(rand.nextInt(100));
+
+        //java.lang.Math.Random;
+        System.out.println(Math.random());//[0.0,1.0)
+
+
+
+
+
+
+
+
+
+
+
+
 
 	}
 }
