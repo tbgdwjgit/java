82ae727da9531a21ef428e384faaa71cdcb01716
tbgdwj <17049185@qq.com>
2017-02-28 15:55:55 +0800

    modify

---
+++ StringTest.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class StringTest{
+	public static void main(String args[]){
+        String a = "abc";
+        String b = "abc";
+        System.out.println(a==b);//true
+        System.out.println(a.equals(b));//true
+
+        String c = new String("abc");
+        String d = new String("abc");
+        System.out.println(c==d);//false
+        System.out.println(c.equals(d));//true
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("abcd");
+        sb.append("www");
+        System.out.println(sb.toString());
+
+
+        Date now = new Date();
+        System.out.println(getDate(now));
+
+
+	}
+
+        public static String getDate(Date date){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
+            String str = sdf.format(date);   
+            return str;                  
+        }
+}
