99018e33ce3b7ca786c61aad10b7d91393b94f6d
tbgdwj <17049185@qq.com>
2017-02-28 10:30:28 +0800

    modify ForTest.java

--- ForTest.java
+++ ForTest.java
@@ -3,12 +3,24 @@ public class ForTest{
     	  int i = 0;
         for(foo('B'); foo('A') && (i < 2); foo('D')) {
             i++;
-            foo('C');
-        }	  
-	  }
+            foo('C');   
+        }
+
+        String tmp="";
+        String arr[]={"This","is","a","test","!"};
+        //foreach
+        for(String str :arr){
+            if (str=="This"){
+               tmp=tmp+str; 
+            }else{
+                tmp=tmp+" "+str;
+            }
+        } 
+        System.out.print(tmp);
+	}
 	
 	  public static boolean foo(char c) {
-        System.out.print(c);
+        System.out.println(c);
         return true;
     }
 }
