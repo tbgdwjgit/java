cd13a7b889d225b51fccad071828847395304469
tbgdwj <17049185@qq.com>
2017-02-28 15:23:09 +0800

    modify

--- ListTest.java
+++ ListTest.java
@@ -3,11 +3,19 @@ import java.util.*;
 public class ListTest{
 
     public static void main(String args[]){
+        //List
+        List<String> list = new ArrayList<String>();
+        list.add("123");
+
     	//ArrayList
         ArrayList<Integer> al = new ArrayList<Integer>(); 
         al.add(321);
         al.add(123);
         al.add(231);
+        // al.set(1,222);
+        // al.remove(1);
+        Collections.sort(al);
+
         //System.out.println(al.size());
         Iterator<Integer> iter1 = al.iterator();  
         while(iter1.hasNext()){  
@@ -26,14 +34,35 @@ public class ListTest{
             System.out.println(iter2.next());  
         } 
         
-
-
-
-
-
-
-
     	//LinkedList
+        LinkedList<String> al2 = new LinkedList<String>();
+        al2.add("This");
+        al2.add("is");
+        al2.add("an");
+        al2.add("apple");
+        al2.add("!!");
+        Iterator<String> iter3 = al2.iterator();  
+        while(iter3.hasNext()){  
+            System.out.println(iter3.next());  
+        } 
+
+        //Vector(ArrayList)
+        Vector<String> Vr = new Vector<String>();
+        Vr.add("This");
+        Vr.add("is");
+        Vr.add("an");
+        Vr.add("apple");
+        Vr.add("!!!");
+        Iterator<String> iter4 = Vr.iterator();  
+        while(iter4.hasNext()){  
+            System.out.println(iter4.next());  
+        } 
+
+
+
+
+
+
 
     }
 
