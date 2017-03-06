615352f4d0d14cd6c28a908450347d0029cde3d4
tbgdwj <17049185@qq.com>
2017-03-06 14:11:56 +0800

    add ClassTest.java

---
+++ ClassTest.java
@@ -0,0 +1,41 @@
+public class ClassTest{
+	public static void main(String args[]){
+    	System.out.println("test");
+	}
+}
+
+class T1{
+
+}
+
+class T2{
+	
+}
+
+class T3{
+	
+}
+
+class T4{
+	
+}
+
+class T5{
+	
+}
+
+class T6{
+	
+}
+
+class T7{
+	
+}
+
+class T8{
+	
+}
+
+class T9{
+	
+}
